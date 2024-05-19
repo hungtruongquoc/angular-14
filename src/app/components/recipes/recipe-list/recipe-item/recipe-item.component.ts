de8ed2ddb94433eb31757a6beb76a742951f9343
Hung Truong <hungtruongquoc@gmail.com>
2024-05-19 16:35:26 -0500

    Unsubscribe unused subscription

--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,24 +1,31 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.sass'
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute) {}
 
   selectedId: number;
+  paramsSubscription: Subscription;
 
   ngOnInit() {
     // this.selectedId = this.route.snapshot.params['id'];
     // console.log(`Item id ${this.route.snapshot.params['id']}`)
-    
+
     // The subscription below is automatically unsubscribed when the component is destroyed
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.selectedId = params['id'];
       console.log(`Item id ${params['id']}`)
     });
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    this.paramsSubscription.unsubscribe();
   }
 }
